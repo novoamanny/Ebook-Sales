4fd9099f67056e082245d3838937b5cd09721deb
Manuel Novoa <manuel.novoa@mavs.uta.edu>
2018-07-09 20:15:43 -0500

    The Frontend of the App is mostly finished. A couple of folders were added (views, public). Handlers were added to the index file that is inside the views folder. Database is not being added with this commit. Payments are accepted and the card verifier is working, but the information is not being stored yet.

--- server.js
+++ server.js
@@ -7,8 +7,22 @@ const app = express();
 const port = process.env.port || 5000;
 
 
+//  Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 // Body Parser
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// static Folder
+app.use(express.static(`${__dirname}/public`));
+
+// Index route
+app.get('/', function(req, res){
+    res.render('index');
+});
+
 
 
 app.listen(port, function(){
