29f71b5079c43c648ef59b0fc27f9d0c0ca507c0
Manuel Novoa <manuel.novoa@mavs.uta.edu>
2018-07-11 17:14:40 -0500

    minor changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const keys = require('./config/keys')
-const stripe = Stripe(keys.stripeSecretKey);
+const stripe = require('stripe')(keys.stripeSecretKey);
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 
